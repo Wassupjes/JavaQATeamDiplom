4e47fc152d59f64f33a473135dd92478c5d7d153
sergei <osp11011983@mail.ru>
2024-03-22 11:48:28 +0100

    add tests

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -40,4 +40,27 @@ public class SavingAccountTest {
 
         Assertions.assertEquals(expected, actual);
     }
+    @Test 
+    public void shouldBalanceAboveTheMin() {
+        SavingAccount account = new SavingAccount(
+                3_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        Assertions.assertTrue(account.pay(2_000));
+    }
+    @Test
+    public void shouldShowExceptionInitialBalance() {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new SavingAccount(500, 1_000, 10_000, 5));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new SavingAccount(11_000, 1_000, 10_000, 5));
+    }
+    @Test
+    public void shouldShowExceptionMinBalance() {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new SavingAccount(2_000, -1_000, 10_000, 5));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new SavingAccount(2_000, 11_000, 10_000, 5));
+    }
 }
